07d0048f3707ccc3d7b9d03e684fc7b1df0f65d5
Pardio11 <l20121022@morelia.tecnm.mx>
2023-11-13 20:58:40 -0600

    first commit

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import './assets/main.css'
+import { createApp } from 'vue'
+import App from './App.vue'
+import axios from 'axios'
+
+const app = createApp(App)
+
+app.config.globalProperties.axios=axios
+
+app.mount('#app')
+
